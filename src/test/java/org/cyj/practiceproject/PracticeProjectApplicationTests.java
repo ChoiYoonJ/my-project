2abe564c13a891861510f7d35694530cff1093fc
ChoiYoonJ <gkt0103@daum.net>
2026-06-18 16:22:03 +0900

    초기 설정

---
+++ src/test/java/org/cyj/practiceproject/PracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyj.practiceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
